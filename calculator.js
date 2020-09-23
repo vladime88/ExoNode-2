66fd6c5f2a63998d014b41d59b66c9368bcaca14
vladime88 <vladime@gmail.com>
2020-09-23 23:22:55 +0200

    calculator?

--- calculator.js
+++ calculator.js
@@ -2,6 +2,24 @@ const sign = process.argv[2]
 const a = Number(process.argv[3])
 const b = Number(process.argv[4])
 
-const result = a + sign + b
+const calc = (sign, a, b) => {
+    const add = (a, b) => a + b
+    const sous = (a, b) => a - b
+    const mul = (a, b) => a * b
+    const div = (a, b) => a / b
 
-console.log(result)
+    switch (sign) {
+        case '+':
+            return add(a, b)
+        case '-':
+            return sous(a, b)
+        case '*':
+            return mul(a, b)
+        case '/':
+            return div(a, b)
+        default:
+            break
+    }
+}
+
+console.log(calc(sign, a, b))
